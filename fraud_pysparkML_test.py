d48ddfabfc63188ea810ada7b05b1ef90b3c6fcf
Ignacio Arroyo <axayakac@gmail.com>
2018-11-30 11:22:40 -0600

    last version spark ML script

--- fraud_pysparkML_test.py
+++ fraud_pysparkML_test.py
@@ -1,15 +1,13 @@
-from pyspark.context import SparkContext
-from pyspark.sql.types import IntegerType
+﻿from pyspark.sql.types import IntegerType
 from pyspark.sql import SparkSession
-from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.evaluation import BinaryClassificationEvaluator as Evaluator
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.ml.classification import LogisticRegression
 import warnings
 import pyspark
-import platform
-#from pyspark.sql import Row
+import argparse
+
 
 spark = SparkSession \
     .builder \
@@ -17,14 +15,19 @@ spark = SparkSession \
     .config("spark.some.config.option", "some-value") \
     .getOrCreate()
 
-sc = SparkContext.getOrCreate()
+## Commands and paths:
 ## spark-submit C:\Users\HCAOA911\Documents\PythonScripts\csv_toMLLIB.py 
-## C:\\Users\\HCAOA911\\Desktop\\data\\fraud_data_sample.csv
+## spark-submit --master spark://15.10.155.75:7077 C:\\Users\\HCAOA911\\Desktop\\data\\fraud_data_sample.csv
+## in_file = "C:\\Users\\HCAOA911\\Desktop\\data\\PS_20174392719_1491204439457_log.csv"
+## in_file = "../data/PS_20174392719_1491204439457_log.csv"
+## in_file = "C:\\Users\\HCAOA911\\Desktop\\data\\fraud_data_sample.csv"
+
 warnings.filterwarnings('ignore')
 
 def vectorizeData(data, validCols, labelsCol):
-    #return data.rdd.map(lambda r: [int(r[-1]), Vectors.dense(r[:-1])]).toDF(['label','features'])
-
+    """Creates dataset from spark DataFrame getting only two columns 'label' 
+    and 'features'. Alist of valid column names must be provided in 
+    'validCols' and the original name of the labels in 'labelsCol'."""
     vectorizer = VectorAssembler(
         inputCols=validCols,
         outputCol='features'
@@ -40,30 +43,30 @@ def labelData(data):
     return data.rdd.map(lambda row: LabeledPoint(row[-1], row[:-1])).toDF()
 
 
-#in_file = "C:\\Users\\HCAOA911\\Desktop\\data\\PS_20174392719_1491204439457_log.csv"
-if platform.system() != 'Linux':
-    in_file = "data\\fraud_data_sample.csv"
-else:
-    in_file = "data/fraud_data_sample.csv"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test fraud detection script in "
+                                                    "credit card payment.")
+    parser.add_argument("--csv", help = "Input file containing features and "
+                                                        "labels in columns")
+    args = parser.parse_args()
 
-CV_data = spark.read.csv(in_file, header=True, inferSchema=True) 
-to_vectorize = ['step','amount','oldbalanceOrg','newbalanceOrig',
+    in_file = args.csv
+    CV_data = spark.read.csv(in_file, header=True, inferSchema=True) 
+    to_vectorize = ['step','amount','oldbalanceOrg','newbalanceOrig',
                 'oldbalanceDest','newbalanceDest','isFlaggedFraud']
 
-training_data, testing_data = CV_data.randomSplit([0.8, 0.2])
+    training_data, testing_data = CV_data.randomSplit([0.8, 0.2])
 
 #xytrain = labelData(training_data)
 
-xytrain = vectorizeData(training_data, validCols=to_vectorize, labelsCol='isFraud')
+    xytrain = vectorizeData(training_data, validCols=to_vectorize, labelsCol='isFraud')
+    lr = LogisticRegression(regParam=0.01)
+    model = lr.fit(xytrain)
 
-#model = LogisticRegressionWithLBFGS.train(xytrain)
-lr = LogisticRegression(regParam=0.01)
-model = lr.fit(xytrain)
+    xytest = vectorizeData(testing_data, validCols=to_vectorize, labelsCol='isFraud')
+    predicted_train = model.transform(xytrain)
+    predicted_test = model.transform(xytest)
+    evaluator = Evaluator()
 
-xytest = vectorizeData(testing_data, validCols=to_vectorize, labelsCol='isFraud')
-predicted_train = model.transform(xytrain)
-predicted_test = model.transform(xytest)
-evaluator = Evaluator()
-
-print("Train %s: %f" % (evaluator.getMetricName(), evaluator.evaluate(predicted_train)))
-print("Test %s: %f" % (evaluator.getMetricName(), evaluator.evaluate(predicted_test)))
+    print("Train %s: %f" % (evaluator.getMetricName(), evaluator.evaluate(predicted_train)))
+    print("Test %s: %f" % (evaluator.getMetricName(), evaluator.evaluate(predicted_test)))
