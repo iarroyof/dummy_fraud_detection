e9e03f089634a3ef95ffdc81efce93bccd570f27
Nacho <iaf@ccg.unam.mx>
2018-11-30 10:48:19 -0600

    the DB connection script is ready

--- pyspark_learn_from_db.py
+++ pyspark_learn_from_db.py
@@ -1,24 +1,24 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
 
-
+# "bin/sqljdbc42.jar") 
 spark = SparkSession \
     .builder \
     .appName("Python Spark SQL basic example") \
-    .config("spark.driver.extraClassPath","bin/sqljdbc42.jar") \
+    .config("spark.driver.extraClassPath", "/almac/ignacio/spark-2.4.0-bin-hadoop2.7/jars/jdbc-postgresql.jar") \
     .getOrCreate()
 
 ## DATABASE: semantica, csdwhpr1
 #url = "jdbc:sqlserver://15.10.154.77\csdwhpr1;database=semantica;user=ReportUser;password=ReportUser"
-query = "select * from semapp.hc_recibo where rownum < 10"
+query = "select * from semapp.fraud_data_sample"
 df = spark.read \
     .format("jdbc") \
-    .option("url", "jdbc:postgres:localhost:5432") \
-    .option("dbtable", "schema.hc_recibo") \
+    .option("url", "jdbc:postgresql:postgres") \
+    .option("dbtable", "semapp.fraud_data_sample") \
     .option("user", "postgres") \
     .option("password", "postgresql") \
     .load()
 
 df.show()
-df = spark.sql(query)
-df.show()
+#df = spark.sql(query)
+#df.show()
