6d83d97c8f2c692f2e0bb4ac012e72d2f1a02706
NachoS <casa@remota.com>
2019-01-04 18:19:23 -0600

    k-fold Stratified balanced Dataset creator from db

--- sliding_leave_estimator.py
+++ sliding_leave_estimator.py
@@ -24,13 +24,24 @@ def get_auth(conn_info="connection.json", db_type="ORACLE", service="DWHPR1"):
                 'service':  j[db_type][service]['URL'].split(':')[5]}
 
 
+def df_hstack(df_list):
+    stacked = df_list[0].withColumn("g_id", F.lit(0))
+    for i, next_df in enumerate(df_list[1:]):
+        stacked = stacked.union(next_df.withColumn("g_id", F.lit(i + 1)))
+
+    return stacked
+
 #jdbcDatabase = "DWHRAW.S_PEN_SOBREVIVENCIA"
 # Tal vez si el nombre del titular es diferente al del asegurado 
 # tenga cierta relevancia como feature
 LEAVE = "Death"
 leave = "MUERTO"
 relation = "nomina"  # "voz", "todo"
-
+# This is a directory for spark. 
+n_groups = 4
+out_csv = "/DB_PQ/pyrthon_data/_data/random_sample_pensiones_{}-fold".format(n_groups) 
+#out_csv = None
+out_null_percent_csv = out_csv + ('' if out_csv is None else "_null_percents")
 remove = [
 	"CALLE",
 	"CELULAR",
@@ -91,7 +102,8 @@ spark = SparkSession \
     .builder \
     .appName("Python Spark SQL basic example") \
     .getOrCreate()
-
+# Avoid INFO logs
+spark.sparkContext.setLogLevel('WARN')
 # A number of top rows for testing
 get_n = 1000
 
@@ -112,7 +124,6 @@ for f in dfa.columns:
 dfb = dfb.drop(*repeats)
 df = dfa.join(dfb, list(chain(*pivot.values()))[0], "right")
 
-printc("%s\n" % df.columns)
 # Remove uninformative columns and putting label LEAVE = "Death"
 valids = [v for v in df.columns if not v in remove]
 df = df.select(valids).orderBy(list(chain(*pivot.values()))[0]) \
@@ -125,45 +136,76 @@ df = df.select(valids).orderBy(list(chain(*pivot.values()))[0]) \
                                    ) \
                       .drop(leave)# \
                       #.drop(*holder_insured)
-
-# Equalize class imbalance while stratified sampling
+# Drop uninformative features (most samples are null for them)
 N = float(df.count())
+# Equalize class imbalance while stratified sampling
 Np =  float(df.filter(F.col(LEAVE) == 1).count())
 Nn =  float(df.filter(F.col(LEAVE) == 0).count())
 
-dfs = df.sampleBy(LEAVE, fractions={0:  Np / N, 1:  Nn / N}, seed=0)
-train, test = dfs.randomSplit([0.7, 0.3], seed=12345)
+if n_groups > 1:
+    # Create multiple ('n_groups') of seeded random datasets
+    groups = [df.sampleBy(LEAVE, fractions={0:  Np / N, 1:  Nn / N}, seed=g)
+                                                       for g in range(n_groups)]
+    if not out_csv is None:
+        df_hstack(groups).coalesce(1) \
+                         .write.option("inferSchema", "true") \
+                         .csv(out_csv, header =True, 
+                                       dateFormat="yyyy-MM-dd HH:mm:ss")
+    else:
+        df_hstack(groups).show()
 
-printc("train Positive n_samples: %f\n"
-       "train Negative n_samples: %f\n"  
-       "test Positive n_samples: %f\n"
-       "test Negative n_samples: %f" \
-       .format(train.filter(F.col(LEAVE) == 1).count(),
-               train.filter(F.col(LEAVE) == 0).count(),
-               test.filter(F.col(LEAVE) == 1).count(),
-               test.filter(F.col(LEAVE) == 0).count()
-               )
-         )
-dfs.coalesce(1).write.option("inferSchema", "true") \
-               .csv("/DB_PQ/pyrthon_data/_data/random_sample_pensiones", 
-                    header =True, dateFormat="yyyy-MM-dd HH:mm:ss")
+else:        
+    dfs = df.sampleBy(LEAVE, fractions={0:  Np / N, 1:  Nn / N}, seed=0)
+    if not out_csv is None:
+        dfs.coalesce(1).write.option("inferSchema", "true") \
+                       .csv(out_csv, header =True, 
+                                       dateFormat="yyyy-MM-dd HH:mm:ss")
+    else:
+        dfs.show()
+
+missing_stats_data = spark.createDataFrame([(c, 1 - float(df.filter(F.col(c).isNull()).count()) / N)
+                                                 for c in df.columns], 
+                                           ["feature", "miss_percent"]
+		                           ).orderBy("miss_percent")
+if not out_null_percent_csv is None:
+    missing_stats_data.coalesce(1).write.option("inferSchema", "true") \
+                   .csv(out_null_percent_csv, header =True)
+    printc("Missing Stats written into %s" % out_null_percent_csv)
+else:
+    missing_stats_data.show()
+
+missing_stats_data.orderBy("miss_percent", ascending=False) \
+                  .limit(int(missing_stats_data.count() * 0.2)).show()
+##train, test = dfs.randomSplit([0.7, 0.3], seed=12345)
+
+#printc("train Positive n_samples: %f\n"
+#       "train Negative n_samples: %f\n"  
+#       "test Positive n_samples: %f\n"
+#       "test Negative n_samples: %f" \
+#       .format(train.filter(F.col(LEAVE) == 1).count(),
+#               train.filter(F.col(LEAVE) == 0).count(),
+#               test.filter(F.col(LEAVE) == 1).count(),
+#               test.filter(F.col(LEAVE) == 0).count()
+#               )
+#         )
+#printc("%s\n" % dfs.columns)
 #dfs.toPandas().to_csv("/DB_PQ/pyrthon_data/_data/random_sample_pensiones.csv", header=True)
-#df.createOrReplaceTempView("DATA")
-#query = "select * from DATA where {} is not null".format(leave)
-#df_p = spark.sql(query)
+##df.createOrReplaceTempView("DATA")
+##query = "select * from DATA where {} is not null".format(leave)
+##df_p = spark.sql(query)
 
-#printc("%s" % df.columns)
-#df.select(list(chain(*pivot.values())) + [holder, insured, hol_ins_col, LEAVE]).filter(F.col(hol_ins_col) == 0).show()
+##printc("%s" % df.columns)
+##df.select(list(chain(*pivot.values())) + [holder, insured, hol_ins_col, LEAVE]).filter(F.col(hol_ins_col) == 0).show()
 
 
 # Verify dimensionality number of samples and class imbalance
-#N = float(df.count())
-#Np =  float(dfp.count())
-#Nn =  float(dfn.count())
-#printc("DF:\nDimensionality: {}\tNumber of samples: {}\n".format(len(df.columns), N))
-#printc("DF_POSITIVE CLASS:\nDimensionality: {}\tNumber of samples: {}\n".format(len(dfp.columns), Np))
-#printc("DF_NEGATIVE CLASS:\nDimensionality: {}\tNumber of samples: {}\n".format(len(dfn.columns), Nn))
-#printc("Class imbalance: P: {}% N: {}%".format(100 * Np / N, 100 * Nn / N))
+##N = float(df.count())
+##Np =  float(dfp.count())
+##Nn =  float(dfn.count())
+##printc("DF:\nDimensionality: {}\tNumber of samples: {}\n".format(len(df.columns), N))
+##printc("DF_POSITIVE CLASS:\nDimensionality: {}\tNumber of samples: {}\n".format(len(dfp.columns), Np))
+##printc("DF_NEGATIVE CLASS:\nDimensionality: {}\tNumber of samples: {}\n".format(len(dfn.columns), Nn))
+##printc("Class imbalance: P: {}% N: {}%".format(100 * Np / N, 100 * Nn / N))
 
 # Now generate windows
 # Verificar si existen varios registros para LEAVE = 0 y solo uno o pocos para LEAVE = 1
